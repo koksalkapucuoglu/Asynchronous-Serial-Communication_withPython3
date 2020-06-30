bbac2424e67629dd92113c467c14cc2344391cd3
Köksal Kapucuoğlu <31320234+koksalkapucuoglu@users.noreply.github.com>
2020-07-01 01:05:05 +0300

    convert python3 code

--- haberlesme_ornek_kod.py
+++ haberlesme_ornek_kod.py
@@ -5,21 +5,21 @@ import time
 
 ser=serial.Serial('/dev/ttyS1',9600)
 ser.write('\nMerhaba Lutfen [y] veya [n] karakterlerinden birini tuslayiniz: ')
-print "Seri Haberlesme basliyor\n"
+print("Seri Haberlesme basliyor\n")
 
 while 1:
         karakter=ser.read()
 
         if karakter == 'y':
-                print "\nYes denildi\n"
+                print("\nYes denildi\n")
                 ser.write( '\n[y] karakterini tusladiniz\n')
         elif karakter == "n":
-                print "\nNo denildi\n"
+                print("\nNo denildi\n")
                 ser.write('\n[n] karakterini tusladiniz \n')
 
 
         else:
-                print "\nbelirlenemeyen bir karakter girisi\n"
+                print("\nbelirlenemeyen bir karakter girisi\n")
                 ser.write('\nGirilen karakter =>')
                 ser.write(karakter)
                 ser.write('\nKarakter belirlenemedigi icin cevap alinmadi. [y]veya [n] karakterlerinden birini girin\n')
